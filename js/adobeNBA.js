93409b126d32fe66684b2b7dbcfe677a4a562251
Jon Shek <jon.shek@branch.io>
2020-09-08 14:24:53 -0700

    yolo

--- js/adobeNBA.js
+++ js/adobeNBA.js
@@ -73,50 +73,6 @@ function createAdobeLink() {
   }
 }
 
-import {API_KEY, client_id } from './gsheets.js';
-
-// Google Sheets API - Append
-function authenticate() {
-		return gapi.auth2.getAuthInstance()
-						.signIn({scope: "https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/drive.file https://www.googleapis.com/auth/spreadsheets"})
-						.then(function() { console.log("Sign-in successful"); },
-												function(err) { console.error("Error signing in", err); });
-}
-function loadClient() {
-		gapi.client.setApiKey(API_KEY);
-		return gapi.client.load("https://content.googleapis.com/discovery/v1/apis/sheets/v4/rest")
-						.then(function() { console.log("GAPI client loaded for API"); },
-												function(err) { console.error("Error loading GAPI client for API", err); });
-}
-// Make sure the client is loaded and sign-in is complete before calling this method.
-function execute(param) {
-		return gapi.client.sheets.spreadsheets.values.append({
-				"spreadsheetId": "18th2014poLYwTbfJjjHlR2LgW9GkNJza6yQzNxWrPms",
-				"range": "A2",
-				"includeValuesInResponse": true,
-				"insertDataOption": "INSERT_ROWS",
-				"responseDateTimeRenderOption": "FORMATTED_STRING",
-				"responseValueRenderOption": "FORMATTED_VALUE",
-				"valueInputOption": "RAW",
-				"resource": {
-						"range": "A2",
-						"values": [
-								[
-										param
-								]
-						]
-				}
-		})
-						.then(function(response) {
-														// Handle the results here (response.result has the parsed body).
-														console.log("Response", response);
-												},
-												function(err) { console.error("Execute error", err); });
-}
-gapi.load("client:auth2", function() {
-		gapi.auth2.init({client_id: client_id});
-});
-
 function storeParams() {
   // var adobeParams = {"adobeParams": { "cid":[], "iid":[], "ls":[] }}
   var adobeJSON = JSON.parse(localStorage.getItem("adobeParams"));
